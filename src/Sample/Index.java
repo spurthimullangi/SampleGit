c4437be865c66476c2bf1340980d131cded4bcaa
spurthimullangi <tspurthi@gmail.com>
2017-09-07 11:17:54 +0800

    added SampleGit

---
+++ src/Sample/Index.java
@@ -0,0 +1,15 @@
+package Sample;
+
+public class Index {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is sample project.");
+
+	}
+
+}
